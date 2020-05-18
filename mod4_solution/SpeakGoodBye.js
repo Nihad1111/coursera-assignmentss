3b66a3cfaaef966a2c6f0bed8d0b5df6d4704129
Nihad <nabdulelizade1025@gmail.com>
2020-05-18 20:51:18 +0400

    mod4

---
+++ mod4_solution/SpeakGoodBye.js
@@ -0,0 +1,12 @@
+(function() {
+
+    var byeSpeaker = {};
+
+    var speakWord = "Good Bye";
+    byeSpeaker.speak = function() {
+
+        console.log(speakWord + " " + byeSpeaker.name);
+    }
+
+    window.byeSpeaker = byeSpeaker;
+})();
