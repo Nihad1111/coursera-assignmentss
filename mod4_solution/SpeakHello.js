ec0df32ffa17b50d74cef24bb8229f22d775a778
Nihad1111 <60701152+Nihad1111@users.noreply.github.com>
2020-05-18 21:28:01 +0400

    Update SpeakHello.js

--- mod4_solution/SpeakHello.js
+++ mod4_solution/SpeakHello.js
@@ -1,8 +1,8 @@
-(function() {
+(function(window) {
     var helloSpeaker = {};
     var speakWord = "Hello";
-    helloSpeaker.speak = function() {
-        console.log(speakWord + " " + helloSpeaker.names);
+    helloSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
     }
     window.helloSpeaker = helloSpeaker;
-})();
+})(window);
